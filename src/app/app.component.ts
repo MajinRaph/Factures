b2f6b9a2fd35f4b252fa9b599f82171cc7f73b89
Raph <raphael.goncalves@live.fr>
2017-11-05 08:55:49 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './providers/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class AppComponent {
+  private isLoggedIn: Boolean;
+  private user_displayName: String;
+  private user_email: String;
+  constructor(public authService: AuthService, private router: Router) {
+    this.authService.afAuth.auth.onAuthStateChanged(
+      (auth) => {
+        if (auth != null) {
+            // User is logged in, use the user object for its info.
+            this.isLoggedIn = true;
+            this.user_displayName = auth.displayName;
+            // etc.
+        } else {
+            // User is not logged in, redirect to where you need to.
+          console.log("Logged out");
+          this.isLoggedIn = false;
+          this.user_displayName = '';
+          this.user_email = '';
+          this.router.navigate(['login']);
+        }
+      }
+    );
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import { HttpModule } from '@angular/http';
+
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+
 import { AppComponent } from './app.component';
+import { AuthService } from './providers/auth.service';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAd8CgbeCQqFp1cf0ypatv7IfqxBrNWiAg",
+    authDomain: "facture-25db5.firebaseapp.com",
+    databaseURL: "https://facture-25db5.firebaseio.com",
+    projectId: "facture-25db5",
+    storageBucket: "facture-25db5.appspot.com",
+    messagingSenderId: "231919835896"
+  };
+
+  const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [AuthService,AngularFireAuth],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
